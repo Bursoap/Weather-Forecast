46030d7b3dd6f98f1be016dd47b06af4384e9625
Bursoap <andrei_kulikov@icloud.com>
2019-10-25 10:49:49 +0300

    setted url in form

--- apps/forecast/routes.py
+++ apps/forecast/routes.py
@@ -2,4 +2,5 @@ from apps.forecast.views import ForecastTemplateView
 
 
 def register_routes(bp):
-    bp.add_url_rule('', view_func=ForecastTemplateView.as_view('forecast'))
+    bp.add_url_rule('', view_func=ForecastTemplateView.as_view('forecasts'),
+                    endpoint='forecasts')
